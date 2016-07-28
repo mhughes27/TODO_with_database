4f58193a7df69dd3e9ae22cdc7551851cd8061f3
Matt Hughes <mhughes27@gmail.com>
2016-07-28 10:59:45 -0500

    simple methods to retrieve data from datatbase

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 MyApp.get "/" do
 
-	erb :"home"
+@tasks = Tasks.new.all
+@bob = Tasks.new.bob
+@complete = Tasks.new.complete
+# @add = Tasks.new.add_task
+
+
+erb :"home"
 
 end
---
+++ app/models/tasks.rb
@@ -0,0 +1,20 @@
+class Tasks
+	def all 
+		DB.execute("SELECT * FROM tasks;")
+	end
+
+	def bob
+		DB.execute("SELECT * FROM tasks WHERE name=\"Bob\";")
+	end
+
+	def complete
+		DB.execute("SELECT * FROM tasks WHERE status=\"complete\";")
+	end
+
+	# def add_task()
+	# 	DB.execute("INSERT INTO tasks (name, description, status) VALUES name=\"Lisa\", description=\"Feed the fish\", status=\"incomplete\";")
+	# end
+
+
+# the class end
+end
